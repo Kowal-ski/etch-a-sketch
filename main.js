5cc2c70caf09c997b66802529e143fcc0d63badb
Kowal-ski <np.40027072@gmail.com>
2022-07-13 13:04:05 +0100

    Optimise draw function
    Use event delegation to remove total number
    of event listeners by removing draw function
    event listener for each cell and add event
    listener with draw function to grid-container.

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-function createGird() {
-    for (x = 1; x <= 16; x++) {
+function createGird(size) {
+    for (x = 1; x <= size; x++) {
         let column = document.createElement('div');
         column.setAttribute('class', 'column');
+        column.setAttribute('id', 'column' + x);
         document.querySelector('.grid-container').appendChild(column);
-        for (y = 1; y <= 16; y++) {
+        for (y = 1; y <= size; y++) {
             let cell = document.createElement('div');
             cell.setAttribute('class', 'cell');
-            cell.addEventListener('mouseover', () => {
-                if (mouseDown == true) {
-                    cell.style.background = 'black';
-                }
-            })
-            cell.addEventListener('mousedown', () => {
-                cell.style.background = 'black';
-            })
+            cell.setAttribute('id', 'cell' + y);
             column.appendChild(cell);
         }
     }
@@ -23,4 +17,13 @@ let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-createGird();
+document.querySelector('.grid-container').addEventListener('mouseover', (e) => {
+    if (mouseDown == true) {
+        e.target.style.background = 'black';
+    }
+})
+document.querySelector('.grid-container').addEventListener('mousedown', (e) => {
+    e.target.style.background = 'black';
+})
+
+createGird(64);
